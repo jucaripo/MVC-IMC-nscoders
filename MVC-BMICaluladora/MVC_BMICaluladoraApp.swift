cbd83f405d8a931d81d90e1414694dd134aad72b
jucaripo <jucaripo@hotmail.com>
2024-03-20 21:04:22 -0600

    Initial Commit

---
+++ MVC-BMICaluladora/MVC_BMICaluladoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVC_BMICaluladoraApp.swift
+//  MVC-BMICaluladora
+//
+//  Created by Juan Carlos Ricalde Poveda on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MVC_BMICaluladoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
